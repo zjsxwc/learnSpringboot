8b907ebdac57891dc72da6358f78346bf087fced
wangchao <zjsxwc@qq.com>
2021-11-03 09:49:29 +0800

    fix controller

--- src/main/java/com/example/demo/TestController.java
+++ src/main/java/com/example/demo/TestController.java
@@ -8,12 +8,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  *
  * @author wangchao
  */
-@Controller
+@RestController
 public class TestController {
     
     @Autowired
